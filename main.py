7bd3e02c23c43f7366daaf22e49d3e1f45fb0fd4
tkdlrs <tkd.lsanchez@gmail.com>
2024-12-30 16:54:39 -0700

    sets up circleshape class

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 def main():
   pygame.init()
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+  clock = pygame.time.Clock()
+  dt = 0  
   # Game loop 
   while True:
     # set up the 'close window' button
@@ -11,11 +13,12 @@ def main():
       if event.type == pygame.QUIT:
         return
     # set up the screen 
-    pygame.Surface.fill(screen, "black")
+    screen.fill("black")
     pygame.display.flip()
-  # print("Starting asteroids!")
-  # print(f"Screen width: {SCREEN_WIDTH}")
-  # print(f"Screen height: {SCREEN_HEIGHT}")
+    # 
+    delta_time = clock.tick(60)
+    dt = delta_time / 1000 
+
 
 
 if __name__ == "__main__":
