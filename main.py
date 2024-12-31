ea9f94a3f61c9f6ca3f4d96cc4db84cd621045aa
tkdlrs <tkd.lsanchez@gmail.com>
2024-12-31 13:10:51 -0700

    Sets up groups for updatable and drawable

--- main.py
+++ main.py
@@ -6,19 +6,31 @@ def main():
   pygame.init()
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
   clock = pygame.time.Clock()
-  player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) 
-  dt = 0
   #
+  updatable = pygame.sprite.Group()
+  drawable = pygame.sprite.Group()
+
+  Player.containers = (updatable, drawable)
+  
+  player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) 
+
+  dt = 0
+
   # Game loop 
   while True:
     # set up the 'close window' button
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         return
+    #
+    for obj in updatable:
+      obj.update(dt)
     # set up the screen 
     screen.fill("black")
-    player.update(dt)
-    player.draw(screen)
+    #
+    for obj in drawable:
+      obj.draw(screen)
+      
     pygame.display.flip()
 
     # limit the framerate to 60 FPS 
