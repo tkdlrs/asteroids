f0618d8fabe1b17aa853aceafd5e93aa9d854f5a
tkdlrs <tkd.lsanchez@gmail.com>
2024-12-31 14:02:18 -0700

    Sets up the astroids and astroid field.

--- main.py
+++ main.py
@@ -1,42 +1,47 @@
 import pygame
-from constants import * 
+from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
 
 def main():
-  pygame.init()
-  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-  clock = pygame.time.Clock()
-  #
-  updatable = pygame.sprite.Group()
-  drawable = pygame.sprite.Group()
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
 
-  Player.containers = (updatable, drawable)
-  
-  player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
-  dt = 0
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
 
-  # Game loop 
-  while True:
-    # set up the 'close window' button
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-        return
-    #
-    for obj in updatable:
-      obj.update(dt)
-    # set up the screen 
-    screen.fill("black")
-    #
-    for obj in drawable:
-      obj.draw(screen)
-      
-    pygame.display.flip()
+    Player.containers = (updatable, drawable)
 
-    # limit the framerate to 60 FPS 
-    dt =  clock.tick(60) / 1000 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        for obj in updatable:
+            obj.update(dt)
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
-  main()
-
+    main()
--- player.py
+++ player.py
@@ -10,7 +10,6 @@ class Player(CircleShape):
   def draw(self, screen):
     pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
-  # in the player class
   def triangle(self):
     forward = pygame.Vector2(0, 1).rotate(self.rotation)
     right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
